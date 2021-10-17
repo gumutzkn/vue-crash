4286c64393203090e140d49b75dca1a7d6a89f6a
Görkem Umut Özkan <gorkem@Gorkem-MacBook-Air.local>
2021-10-17 15:35:43 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      :text="!showAddTask ? 'Add Task' : 'Close'"
+      :color="!showAddTask ? 'green' : 'red'"
+      @btn-click="$emit('btn-click')"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  emits: ["btn-click"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
